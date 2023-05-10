d889067c91ddf1ff65cc8884149b39814bbaacfd
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-05-10 21:37:41 +0300

    building first custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
